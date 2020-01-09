01a45d6a66beca2fe7f395cc4cd021dcc78dcc75
Alexey Kolchin <murrel@yandex.ru>
2020-01-09 04:40:34 +0300

    Version 0.0.3

--- src/Controller/MainPageController.php
+++ src/Controller/MainPageController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+use Mobile_Detect;
 
 class MainPageController extends AbstractController
 {
@@ -12,6 +15,13 @@ class MainPageController extends AbstractController
       * @Route("/")
       */
      public function index(){
-         return $this->render('MainPage.html.twig');
+         return $this->render('index.html.twig');
+     }
+
+    /**
+     * @Route("/game/{game_id}", name="game_page")
+     */
+     public function game(string $game_id){
+         return $this->render('game.html.twig',["game_id"=>$game_id]);
      }
 }
