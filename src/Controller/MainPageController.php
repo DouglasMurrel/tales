29dfc804c9d54bea6951e13bf9ac27f4464bb275
Alexey Kolchin <murrel@yandex.ru>
2020-01-10 05:14:07 +0300

    Version 0.0.3

--- src/Controller/MainPageController.php
+++ src/Controller/MainPageController.php
@@ -7,8 +7,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-use Mobile_Detect;
-
 class MainPageController extends AbstractController
 {
      /**
