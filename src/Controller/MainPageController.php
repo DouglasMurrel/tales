5cd640071749e945871f6f7c03832f913a29f920
Alexey Kolchin <murrel@yandex.ru>
2020-01-13 04:24:18 +0300

    Version 0.0.4

--- src/Controller/MainPageController.php
+++ src/Controller/MainPageController.php
@@ -10,9 +10,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class MainPageController extends AbstractController
 {
      /**
-      * @Route("/")
+      * @Route("/", name="main_page")
       */
      public function index(){
+         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_REMEMBERED');
          return $this->render('index.html.twig');
      }
 
@@ -20,6 +21,7 @@ class MainPageController extends AbstractController
      * @Route("/game/{game_id}", name="game_page")
      */
      public function game(string $game_id){
+         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_REMEMBERED');
          return $this->render('game.html.twig',["game_id"=>$game_id]);
      }
 }
