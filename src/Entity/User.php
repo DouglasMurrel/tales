53f1e83fae788f0a742684f2eebc3b3082ae8ede
Alexey Kolchin <murrel@yandex.ru>
2020-01-14 02:49:56 +0300

    Version 0.0.4

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -35,6 +35,11 @@ class User implements UserInterface
      */
     private $password;
 
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $password_hash;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -111,5 +116,17 @@ class User implements UserInterface
     {
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
+    }
+
+    public function getPasswordHash(): ?string
+    {
+        return $this->password_hash;
+    }
+
+    public function setPasswordHash(?string $password_hash): self
+    {
+        $this->password_hash = $password_hash;
+
+        return $this;
     }
 }
