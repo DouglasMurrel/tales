ebab372612b821f412ff1406cf01509cbbd43a04
Alexey Kolchin <murrel@yandex.ru>
2020-01-15 16:58:07 +0300

    Version 0.0.1

--- config/web.php
+++ config/web.php
@@ -14,7 +14,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'KFANCgNJRfXjFokUgNm0zsb2z40u-Ngx',
+            'cookieValidationKey' => 'xxuD4AlLuCLj5LVleivm3CDBRNSHKJMh',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
--- php.bat
+++
@@ -1,1 +0,0 @@
-m:/openserver/distr/ospanel/modules/php/PHP_7.3-x64/php %1 %2 %3 %4 %5 %6 %7 %8 %9
