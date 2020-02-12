0a4df2b3201a1fb8b0b20a866c2d8ca0461a2bd4
Alexey Kolchin <murrel@yandex.ru>
2020-02-13 02:17:00 +0300

    Version 0.2.4

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -23,16 +23,12 @@ AppAsset::register($this);
     <title><?= Html::encode($this->title) ?></title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <style>
-        html {
-            height:100%;
-        }
         body {
-            height:100%;
             background-color: #BFC9CC;
         }
         .container-lg{
             background-color: white;
-            height: 100%;
+            min-height: 100vh;
         }
         #top{
             height:50px;
